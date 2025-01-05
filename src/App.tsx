046d48fc3da489df93b121747f75dde61b561e9b
Dino <111647675+Enzyklopaedino@users.noreply.github.com>
2025-01-05 11:42:58 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './main.css';
+import { Button } from '@/components/ui/button';
+
+const App = () => {
+	return (
+		<div>
+			<h1 class="text-3xl font-bold underline text-teal-400">
+				Rsbuild with React
+			</h1>
+			<h1 class="text-3xl font-bold underline">Hello world!</h1>
+			<Button>click me!</Button>
+			<p>Start building amazing things with Rsbuild.</p>
+		</div>
+	);
+};
+
+export default App;
