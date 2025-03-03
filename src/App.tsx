9354256c3b0059b7352d7149138638185bbc99a6
Dino <111647675+Enzyklopaedino@users.noreply.github.com>
2025-03-03 16:00:37 +0100

    refactor: rename class to className

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { Button } from '@/components/ui/button';
 const App = () => {
 	return (
 		<div>
-			<h1 class="text-3xl font-bold underline text-teal-400">
+			<h1 className="text-3xl font-bold underline text-teal-400">
 				Rsbuild with React
 			</h1>
-			<h1 class="text-3xl font-bold underline">Hello world!</h1>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
 			<Button>click me!</Button>
 		</div>
 	);
