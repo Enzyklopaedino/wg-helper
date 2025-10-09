9f928ea4d4990bb0c0406e91f64408471e9edbb9
Enzyklopaedino <dino2mail2@gmail.com>
2025-10-09 21:48:29 +0200

    feat: add todo item list and button icon

--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -1,15 +1,31 @@
 import { Checkbox } from '@/components/checkbox';
-import { useState } from 'react';
+import { DeleteButton } from '@/components/delete-button';
+import { Input } from '@/components/input';
+import { FC, InputHTMLAttributes, useState } from 'react';
 
-export const TodoItem = () => {
-	// const [firstName, setFirstName] = useState('');
-	console.log('helo');
+export const TodoItem: FC<InputHTMLAttributes<HTMLInputElement>> = ({
+	value,
+	onKeyDown,
+}) => {
+	const [checked, setChecked] = useState(false);
 
 	return (
-		<div className="space-x-2">
-			{/* <Checkbox className="peer" /> */}
-			{/* <input className="transition-all peer-data-[state=checked]:line-through peer-data-[state=checked]:text-muted-foreground" /> */}
-			<input type="text" name="todo-input" placeholder="Typing... " />
+		<div
+			id={`todo-item-${Date.now()}`}
+			className="flex flex-row items-center space-x-4"
+		>
+			<Checkbox
+				className="peer"
+				onCheckedChange={(value: boolean) => setChecked(value)}
+			/>
+			<Input
+				className="transition-all peer-data-[state=checked]:line-through peer-data-[state=checked]:text-muted-foreground"
+				value={value}
+				onKeyDown={onKeyDown}
+				autoFocus
+				disabled={checked}
+			/>
+			<DeleteButton />
 		</div>
 	);
 };
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,12 +1,57 @@
 'use client';
 import { TodoItem } from '@/components/todo-item';
+import { ChangeEvent, InputHTMLAttributes, useState } from 'react';
+
+// usestate type todos: <InputHTMLAttributes<HTMLInputElement>[]>
+
+export type TodoItemProps = InputHTMLAttributes<HTMLInputElement> & {
+	key?: string;
+};
 
 export const Cart = () => {
-	console.log('parent');
+	const handleKeyDown = (e: any) => {
+		if (e.key === 'Enter') {
+			const newTodo: TodoItemProps = {};
+			console.log('boo');
+			console.log(JSON.stringify([...todos, newTodo]));
+			setTodos([...todos, newTodo]);
+		}
+	};
+
+	const [todos, setTodos] = useState<TodoItemProps[]>([{}]);
+	const [input, setInput] = useState('');
+
+	const handleChange = (value: string, key?: string) => {
+		console.log(value + 'key:' + key);
+		setInput(value);
+	};
+
 	return (
-		<div className="flex justify-center items-center h-screen">
-			{/* <TodoItem /> */}
-			<input type="text" placeholder="meep" />
+		<div className="flex justify-center items-center flex-col space-y-2 h-screen">
+			{/* <TodoItem defaultValue={'helloo'} /> */}
+			<ul className="mt-4 space-y-2">
+				{todos.map((todo) => (
+					<TodoItem key={todo.id} onKeyDown={handleKeyDown} />
+					//				<li
+					//					key={Date.now().toString() + Math.random()}
+					//					className="p-2 border rounded"
+					//				>
+					//					<input
+					//						className="text-slate-900"
+					//						value={input}
+					//						onChange={(e) => handleChange(e.target.value, e.target.accessKey)}
+					//						onKeyDown={handleKeyDown}
+					//					/>
+					//				</li>
+				))}
+			</ul>
+			{/* <TodoItem */}
+			{/* 	text={'foo'} */}
+			{/* 	onChange={(e: ChangeEvent<HTMLInputElement>) => */}
+			{/* 		setInputValue(e.target.value) */}
+			{/* 	} */}
+			{/* /> */}
 		</div>
 	);
 };
+// };
